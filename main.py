f0e935d6d2f159a6a6ed2fcb3b49e206e07b21dc
dmitdub <101890272+dmitdub@users.noreply.github.com>
2022-05-10 19:55:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,22 +5,24 @@ filename = 'results.csv'
 with open(filename) as csvfile:
     reader = csv.reader(csvfile, delimiter="/")
     data = list(reader)
-    number_of_lines = len(data)
-    for i in range(number_of_lines):
-        for index in range(len(data[i])):
-            try:
-                data[i][index] = int(float(data[i][index]))
-            except:
-                pass
+    for row in data:
+        for index in range(1, len(row)):
+            row[index] = int(row[index].split('.')[0])
 
-plt.title('graph of the dependence of the sorting time on the number of elements')
-plt.xlabel('number of sorted items')
-plt.ylabel('time, µs')
+    for i in range(4):
+        plt.subplot(2, 2, i+1)
+        if i == 0: newdata = data
+        if i == 1: newdata = data[0:1] + data[5:6] + data[7:9] + data[10:11]
+        if i == 2: newdata = data[0:1] + data[2:3] + data[4:5]
+        if i == 3: newdata = data[0:2] + data[3:4] + data[6:7] + data[9:10] + data[11:12]
+        for row in newdata:
+            if row != newdata[0]:
+                number_of_elements = len(row)
+                plt.plot(data[0][1:number_of_elements], row[1:number_of_elements], label=row[0], marker='^')
 
-for i in range(number_of_lines - 1):
-    number_of_elements = len(data[i+1])
-    print(number_of_elements)
-    plt.plot(data[0][1:number_of_elements], data[i+1][1:number_of_elements], label=data[i+1][0], marker='^')
+        plt.title('зависимость времени сортировки от количества элементов')
+        plt.xlabel('количество сортируемых элементов')
+        plt.ylabel('время, мкс')
+        plt.legend()
 
-plt.legend()
-plt.show()
+    plt.show()
